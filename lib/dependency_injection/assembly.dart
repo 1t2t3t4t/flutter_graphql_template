17ba4824582fb8de7b29bf7d63cdd5c2fcb988f1
Boss <bossnathakorn@gmail.com>
2021-04-11 01:00:57 +0700

    add basic DI

---
+++ lib/dependency_injection/assembly.dart
@@ -0,0 +1,26 @@
+import 'package:flutter_graphql_template/utils/type/either.dart';
+import 'package:injector/injector.dart';
+
+abstract class Assembly {
+  void register(Injector i);
+}
+
+mixin AssemblyContainer {
+  List<Either<Assembly, AssemblyContainer>> get assemblies;
+}
+
+abstract class CompositeAssembly with AssemblyContainer {
+  void register(Injector i) {
+    _register(i, assemblies);
+  }
+
+  void _register(Injector i, List<Either<Assembly, AssemblyContainer>> a) {
+    for (final assembly in assemblies) {
+      if (assembly.isLeft()) {
+        assembly.asLeft().register(i);
+      } else {
+        _register(i, assembly.asRight().assemblies);
+      }
+    }
+  }
+}
---
+++ lib/utils/type/either.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/cupertino.dart';
+
+class Either<TLeft, TRight> {
+  final TLeft? _left;
+  final TRight? _right;
+
+  Either.left(this._left) : _right = null;
+  Either.right(this._right) : _left = null;
+
+  bool isLeft() => _left != null;
+  bool isRight() => _right != null;
+
+  TLeft asLeft() {
+    assert(_left != null, "Left cannot be null");
+    return _left!;
+  }
+
+  TRight asRight() {
+    assert(_right != null, "Left cannot be null");
+    return _right!;
+  }
+}
